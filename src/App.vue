e3358b1697fefe148db16211e5538cb79e89fe6e
msthill <micah.sthill123@gmail.com>
2026-03-01 17:24:16 -0600

    vmodel vs vbind

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>v-model vs v-bind</h1>
+  <h2>message: {{ message }}</h2>
+  <div>
+    <p>v-model = v-bind + @input</p>
+    <input
+      type="text"
+      v-bind:value="message"
+      @input="message = $event.target.value"
+    />
+  </div>
+  <div>
+    <p>v-model</p>
+    <input type="text" v-model="message" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+import { ref } from 'vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+let message = ref('Hello!')
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
