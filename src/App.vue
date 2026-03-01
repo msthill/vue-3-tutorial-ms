32c4f3941ee33261437e589d7a98543e19481a29
msthill <micah.sthill123@gmail.com>
2026-03-01 17:12:29 -0600

    text interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,102 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <!--The most basic form of data binding is text interpolation using the "Mustache" syntax (double curly braces)-->
+  <h1>message: {{ message }}</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <hr />
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!--{{message}} refers to the message we defined in data-->
+  <h1>number: {{ number }}</h1>
+
+  <h1>doubleNum(50): {{ doubleNum(50) }}</h1>
+
+  <h1>number * 2 = {{ number * 2 }}</h1>
+
+  <h1>
+    {{ number > 150 ? 'number is great than 150' : 'number is less than 150' }}
+  </h1>
+
+  <h1 v-text="number"></h1>
+
+  <hr />
+
+  <h1>harry: {{ harry }}</h1>
+
+  <h1>harry.name: {{ harry.name }}</h1>
+
+  <h1>hogwartsWizards: {{ hogwartsWizards }}</h1>
+
+  <h1>hogwartsWizards[0]: {{ hogwartsWizards[0] }}</h1>
+  <hr />
+
+  <h1>rawHtml: {{ rawHtml }}</h1>
+
+  <h1 v-text="rawHtml"></h1>
+
+  <h1 v-html="rawHtml"></h1>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+let message = 'Hello, Vue!'
+let number = 50
+
+function doubleNum(num) {
+  return num * 2
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+let harry = {
+  id: 1001,
+  name: 'Harry Potter',
+  house: 'Gryffindor',
+  age: 17, // Age during the final battle of Hogwarts
+  wand: {
+    core: 'Phoenix feather',
+    wood: 'Holly'
   }
 }
-</style>
+const hogwartsWizards = [
+  {
+    id: 1001,
+    name: 'Harry Potter',
+    house: 'Gryffindor',
+    age: 17,
+    wand: {
+      core: 'Phoenix feather',
+      wood: 'Holly'
+    }
+  },
+  {
+    id: 1002,
+    name: 'Hermione Granger',
+    house: 'Gryffindor',
+    age: 17,
+    wand: {
+      core: 'Dragon heartstring',
+      wood: 'Vine'
+    }
+  },
+  {
+    id: 1003,
+    name: 'Ron Weasley',
+    house: 'Gryffindor',
+    age: 17,
+    wand: {
+      core: 'Unicorn hair',
+      wood: 'Willow'
+    }
+  },
+  {
+    id: 1004,
+    name: 'Draco Malfoy',
+    house: 'Slytherin',
+    age: 17,
+    wand: {
+      core: 'Dragon heartstring',
+      wood: 'Hawthorn'
+    }
+  }
+]
+
+let rawHtml = '<span style="color: red">This should be red.</span>'
+</script>
+
+<style lang="scss" scoped></style>
