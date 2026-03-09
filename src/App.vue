f5859f91087962a0c5456dca662601933e0f89e6
msthill <micah.sthill123@gmail.com>
2026-03-08 22:50:49 -0500

    computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>{{ message }}</h1>
+  <button @click="sortUsersByAge">Sort users by age</button>
+  <br />
+  <button @click="hideInactive = !hideInactive">{{ toggleButtonName }}</button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <h2>Number of active users (computed property): {{ numberOfActiveUsers }}</h2>
+  <h2>Number of active users (computed property): {{ numberOfActiveUsers }}</h2>
+  <h2>Number of active users (computed property): {{ numberOfActiveUsers }}</h2>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>
+    Number of active users (method call): {{ computeNumberOfActiveUsers() }}
+  </h2>
+  <h2>
+    Number of active users (method call): {{ computeNumberOfActiveUsers() }}
+  </h2>
+  <h2>
+    Number of active users (method call): {{ computeNumberOfActiveUsers() }}
+  </h2>
+  <table>
+    <tr>
+      <th>Index</th>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Age</th>
+      <th>Operation</th>
+    </tr>
+    <tr v-for="(user, index) in filteredUsers" :key="user.id">
+      <td>{{ index + 1 }}</td>
+      <td>{{ user.id }}</td>
+      <td :class="{ inactive: !user.isActive }">
+        {{ user.name }}
+      </td>
+      <td>{{ user.age }}</td>
+      <td>
+        <button @click="user.isActive = !user.isActive">
+          {{ user.isActive ? 'Deactivate' : 'Restore' }}
+        </button>
+      </td>
+    </tr>
+  </table>
 </template>
 
+<script setup>
+import { computed, ref } from 'vue'
+
+let message = ref('Hello, Computed Properties!')
+
+const users = ref([
+  { id: 1001, name: 'John Smith', age: 26, isActive: false },
+  { id: 1002, name: 'Tom Doe', age: 16, isActive: false },
+  { id: 1003, name: 'Frankin Wong', age: 18, isActive: true }
+])
+
+let hideInactive = ref(false)
+
+function sortUsersByAge() {
+  users.value.sort((a, b) => a.age - b.age)
+}
+
+let toggleButtonName = computed(() =>
+  hideInactive.value ? 'Show all' : 'Hide inactive'
+)
+
+let numberOfActiveUsers = computed(() => {
+  console.log('computed property')
+  return users.value.filter((user) => user.isActive).length
+})
+
+let computeNumberOfActiveUsers = () => {
+  console.log('method call')
+  return users.value.filter((user) => user.isActive).length
+}
+
+let filteredUsers = computed(() =>
+  hideInactive.value ? users.value.filter((user) => user.isActive) : users.value
+)
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.inactive {
+  color: red;
+  text-decoration: line-through;
 }
 </style>
