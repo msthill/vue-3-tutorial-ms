48eb0ab38f58e92baafd381b27ba17253155c8c2
msthill <micah.sthill123@gmail.com>
2026-03-13 21:18:55 -0500

    pass data to descendant components with provide and inject

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Message: {{ message }}</h1>
+  <div style="background-color: orange; padding-left: 10px;">
+    <ComponentA></ComponentA>
+  </div>
 </template>
 
+<script setup>
+import { ref , provide, readonly} from 'vue'
+import ComponentA from './ComponentA.vue'
+
+let message = ref('Hello, Provide/Inject!')
+
+function updateMessage() {
+    message.value = 'Hello, Provide/Inject! Updated.'
+}
+
+provide('msg', {message, updateMessage})
+
+
+let count = ref(0)
+provide('read-only-count', readonly(count))
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
