5f0e0f3a5ad4c0a1af022660ea3d70753159dd8d
msthill <micah.sthill123@gmail.com>
2026-03-13 20:54:20 -0500

    pass data to a parent component with custom events

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,45 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <BlogPost v-for="post in posts" :key="post.id" v-bind="post" @delete-blog-post="processDeletion"></BlogPost>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import { ref } from 'vue'
+import BlogPost from './BlogPost.vue'
+let posts = ref([
+  {
+    id: 1,
+    blogPostTitle: 'What is a muggle in HP world?',
+    blogPostContent:
+      'A muggle is a person who lacks any sort of magical ability...'
+  },
+  {
+    id: 2,
+    blogPostTitle:
+      'HP and the Cursed Child Broadway production suspended until April',
+    blogPostContent:
+      'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+  },
+  {
+    id: 3,
+    blogPostTitle: 'Potter DIY: Make Your Own “Harry Potter Puppet Pal”',
+    blogPostContent:
+      'We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...'
+  },
+  {
+    id: 4,
+    blogPostTitle: 'Fan Project Brings Life to “Harry Potter” in Translation',
+    blogPostContent:
+      'It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....'
+  } 
+])
+
+function processDeletion(id){
+  let index = posts.value.findIndex(item => item.id === id)
+  posts.value.splice(index, 1)
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+<style  scoped>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/BlogPost.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="blog-post">
+        <p>{{ message }}</p>
+        <h2>{{id}} - {{blogPostTitle}}</h2>
+        <h4>{{blogPostContent}}</h4>
+        <button @click="$emit('delete-blog-post', id)">Delete post</button>
+        <button @click="emitDeletePostEvent">Delete post</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('This is the BlogPost component.')
+const props = defineProps(['id','blogPostTitle','blogPostContent'])
+const emit = defineEmits(['delete-blog-post']) 
+console.log(props.blogPostTitle)
+//defineProps({
+//  id: Number,
+//  blogPostTitle: String,
+//  blogPostContent: String
+//})
+
+function changeBlogPostTitle() {
+    props.blogPostTitle = 'A different title'
+}
+
+function emitDeletePostEvent(id) {
+    emit('delete-blog-post', id)
+}
+</script>
+
+<style scoped>
+.blog-post {
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+</style>
