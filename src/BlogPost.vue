059a6e7f5aa74fad66d480b8fae2898897d3fbf2
msthill <micah.sthill123@gmail.com>
2026-03-13 21:07:12 -0500

    component v-model for a two-way data binding

---
+++ src/BlogPost.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="blog-post">
+        <p>{{ message }}</p>
+        <h2>{{id}} - {{blogPostTitle}}</h2>
+        <div>
+            Modify the title: <input type="text" v-model="blogPostTitle">
+        </div>
+        <h4>{{blogPostContent}}</h4>
+
+        <div>
+            Modify the content: <input type="text" v-model="blogPostContent">
+        </div>
+        <button @click="$emit('delete-blog-post', id)">Delete post</button>
+        <button @click="emitDeletePostEvent">Delete post</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('This is the BlogPost component.')
+const props = defineProps(['id'])
+const emit = defineEmits(['delete-blog-post']) 
+console.log(props.blogPostTitle)
+//defineProps({
+//  id: Number,
+//  blogPostTitle: String,
+//  blogPostContent: String
+//})
+
+function changeBlogPostTitle() {
+    props.blogPostTitle = 'A different title'
+}
+
+function emitDeletePostEvent(id) {
+    emit('delete-blog-post', id)
+}
+
+let blogPostContent = defineModel('blogPostContent')
+let blogPostTitle = defineModel('blogPostTitle')
+</script>
+
+<style scoped>
+.blog-post {
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+input{
+    width: 100%;
+}
+</style>
