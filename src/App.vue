273dc1d47c54eb272cf6130645a5a617e307912a
msthill <micah.sthill123@gmail.com>
2026-03-13 20:10:10 -0500

    pass data to child components with props

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,68 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div style="background-color: orange"; padding: 5px;>
+    <p> This is the app component</p>
+    <h2>{{ blogPostId }} - {{ blogPostTitle }}</h2>
+    <h4>{{ blogPostContent }}</h4>
+    <button @click="blogPostTitle = 'A different title'">Change blog post title</button>
+    <BlogPost>
+      :id="blogPostId"
+      :blog-post-title="blogPostTitle"
+      :blog-post-content="blogPostContent"
+    </BlogPost>
+    <BlogPost>
+      :id="blogPostId"
+      :blog-post-title="blogPostTitle"
+      :blog-post-content="blogPostContent"
+    </BlogPost>
+  </div>
+  <BlogPost id="1" blogPostTitle="What is a muggle?" blogPostContent="A muggle is a person who lack any sort of magical ability."></BlogPost>
+  <BlogPost id="1" blog-post-title="What is a muggle?" blog-post-content="A muggle is a person who lack any sort of magical ability."></BlogPost>
+  <BlogPost :id="blogPostId" :blog-post-title="blogPostTitle" :blog-post-content="blogPostContent"></BlogPost>
+  <hr />
+  <BlogPost v-for="post in posts" :key="post.id" :id="post.id" :blog-post-title="post.blogPostTitle" :blog-post-content="post.blogPostContent"></BlogPost>
+  <BlogPost v-for="post in posts" :key="post.id" v-bind="post" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+import BlogPost from './BlogPost.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+let blogPostId = ref(2)
+let blogPostTitle = ref('HP and the Cursed Child Broadway production suspended until April')
+let blogPostContent = ref('Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...')
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+let posts = ref([
+  {
+    id: 1,
+    blogPostTitle: 'What is a muggle in HP world?',
+    blogPostContent:
+      'A muggle is a person who lacks any sort of magical ability...'
+  },
+  {
+    id: 2,
+    blogPostTitle:
+      'HP and the Cursed Child Broadway production suspended until April',
+    blogPostContent:
+      'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+  },
+  {
+    id: 3,
+    blogPostTitle: 'Potter DIY: Make Your Own “Harry Potter Puppet Pal”',
+    blogPostContent:
+      'We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...'
+  },
+  {
+    id: 4,
+    blogPostTitle: 'Fan Project Brings Life to “Harry Potter” in Translation',
+    blogPostContent:
+      'It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....'
   }
+])
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+<style  scoped>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
