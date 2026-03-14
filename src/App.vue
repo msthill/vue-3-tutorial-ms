41e7c9282593639af10162361ecdda103826e2ce
msthill <micah.sthill123@gmail.com>
2026-03-13 19:23:24 -0500

    intro to vue components

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <ButtonCounter></ButtonCounter>
+  <ButtonCounter></ButtonCounter>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <button-counter></button-counter>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <hr />
+
+  <div style="background-color: orange; padding: 5px;">
+    This is the root component.
+  <ComponentA></ComponentA>
+  <ComponentA></ComponentA>
+  </div>
 </template>
 
+<script setup>
+import ButtonCounter from './ButtonCounter.vue'
+import ComponentA from '@/ComponentA.vue'
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
