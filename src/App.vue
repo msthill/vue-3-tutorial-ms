a1d8396e81cafdd45f39f9752a201aed0a432c98
msthill <micah.sthill123@gmail.com>
2026-03-13 21:51:46 -0500

    vue scoped slots

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <StudentList :list="list">
+    <template #default="{ stu }">
+      <span :class="{ cursed: stu.name == 'Harry' }">
+        {{ stu.name }}
+      </span>
+    </template>
+  </StudentList>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <hr />
 
-  <main>
-    <TheWelcome />
-  </main>
+  <el-table :data="todoList" stripe border style="width: 100%">
+    <el-table-column prop="userId" label="User ID" width="180" />
+    <el-table-column prop="id" label="ID" width="180" />
+    <el-table-column prop="title" label="Title" />
+    <el-table-column prop="completed" label="Status">
+      <template #default="slotProps">
+        <el-tag type="success" v-if="slotProps.row.completed">Completed</el-tag>
+        <el-tag type="danger" v-else>Incomplete</el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
+<script setup>
+import { ref, onMounted} from 'vue'
+import StudentList from './StudentList.vue'
+
+const list = ref([
+  {
+    id: 1,
+    name: 'Harry'
+  },
+  {
+    id: 2,
+    name: 'Hermione'
+  },
+  {
+    id: 3,
+    name: 'Ron'
+  }
+])
+
+const todoList = ref([])
+
+async function getTodoList() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const data = await response.json()
+  console.log(data)
+  todoList.value = data
+}
+
+onMounted(() => {
+  getTodoList()
+})
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.cursed {
+  color: red;
 }
 </style>
---
+++ src/StudentList.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li v-for="student in list" :key="student.id">
+            {{ student.name }}
+        </li>
+    </ul>
+</template>
+
+<script setup>
+defineProps(['list'])
+</script>
+
+<style scoped>
+
+</style>
