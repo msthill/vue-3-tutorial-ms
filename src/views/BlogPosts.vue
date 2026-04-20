030d96c78ad7c08a35448876ddc60964a8704223
msthill <micah.sthill123@gmail.com>
2026-04-19 21:25:23 -0500

    named views

--- src/views/BlogPosts.vue
+++ src/views/BlogPosts.vue
@@ -4,8 +4,11 @@
         <aside class="blog-post-list">
             <BlogPostList></BlogPostList>
         </aside>
-        <main class="blog-post-content">
-            <router-view></router-view>
+        <main class="blog-post-content-with-sidebar">
+            <!-- This is the default router-view -->
+            <router-view class="blog-post-content"></router-view>
+            <!-- This is the named router-view -->
+            <router-view name="sidebar"></router-view>
         </main>
     </div>
 </template>
@@ -17,14 +20,20 @@ import BlogPostList from '@/components/BlogPostList.vue'
 <style lang="scss" scoped>
 .blog-posts-layout {
     display: flex;
+    width: 100%;
     gap: 1rem;
 
     .blog-post-list {
         flex: 0 0 25%
     }
 
-    .blog-post-content {
+    .blog-post-content-with-sidebar {
+        display: flex;
         flex: 0 0 75%;
+
+        .blog-post-content {
+            flex: 1; // Take up the remaining space
+        }
     }
 }
 </style>
