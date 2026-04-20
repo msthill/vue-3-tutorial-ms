8dc1d741ea9ded1a35b56b81ccbcf2e7a2c529a1
msthill <micah.sthill123@gmail.com>
2026-04-19 21:20:36 -0500

    nested routes and dynamic route matching

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
+
 import { createRouter, createWebHistory } from 'vue-router'
 
 // Import the components that we want to route to
 import Home from '@/views/Home.vue'
 import BlogPosts from '@/views/BlogPosts.vue'
 import About from '@/views/About.vue'
+import BlogPost from '@/views/BlogPost.vue'
+import BlogPostsGreeting from '@/views/BlogPostsGreeting.vue'
+import NotFound from '@/views/NotFound.vue'
 
 // Create a router instance
 const router = createRouter({
@@ -12,8 +16,22 @@ const router = createRouter({
   // Define some routes, each route record should map to a component
   routes: [
     { path: '/', name: 'home', component: Home },
-    { path: '/blog-posts', name: 'blogPosts', component: BlogPosts },
+    {
+      path: '/blogPosts',
+      name: 'blogPosts',
+      component: BlogPosts,
+      redirect: { name: 'blogPostsGreeting' },
+      children: [
+        { path: '', name: 'blogPostsGreeting', component: BlogPostsGreeting },
+        { path: ':id(\\d+)', name: 'blogPost', component: BlogPost },
+      ],
+    },
     { path: '/about', name: 'about', component: About },
+    {
+      path: '/:pathMatch(.*)*', // Match any path that hasn't been matched by a previous route
+      name: 'notFound',
+      component: NotFound,
+    },
   ],
 })
 
